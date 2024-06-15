54c9fb50e947e585133c514e40b05570defacb88
soeunl <soeun318@gmail.com>
2024-06-15 18:42:16 +0900

    교재 10장 문제 풀이

---
+++ Study01/day01/src/Study05/CompleteCalcTest.java
@@ -0,0 +1,10 @@
+package Study05;
+
+public class CompleteCalcTest {
+    public static void main(String[] args) {
+        int num  = 10;
+
+        CompleteCalc calc = new CompleteCalc();
+        System.out.println(calc.square(num));
+    }
+}
---
+++ Study01/day01/src/Study05/QuickSort.java
@@ -0,0 +1,20 @@
+package Study05;
+
+public class QuickSort implements Sort{
+
+    @Override
+    public void ascending(int[] arr) {
+        System.out.println("QuickSort ascending");
+    }
+
+    @Override
+    public void descending(int[] arr) {
+        System.out.println("QuickSort descending");
+    }
+
+    @Override
+    public void description() {
+        Sort.super.description();
+        System.out.println("QuickSort 입니다");
+    }
+}
