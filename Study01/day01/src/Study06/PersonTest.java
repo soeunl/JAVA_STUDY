3e76981a5af033a885d5665653c6f95c67cce573
soeunl <soeun318@gmail.com>
2024-06-15 18:41:48 +0900

    교재 11장 문제 풀이

---
+++ Study01/day01/src/Study06/PersonTest.java
@@ -0,0 +1,32 @@
+package Study06;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class PersonTest {
+    public static void main(String[] args) {
+
+        Class personClass = Person.class;
+
+        Constructor[] cons = personClass.getConstructors();
+        for(Constructor c : cons) {
+            System.out.println(c);
+        }
+
+        System.out.println();
+
+        Method[] methods = personClass.getMethods();
+
+        for(Method m : methods) {
+            System.out.println(m);
+        }
+
+        System.out.println();
+        Field[] fields = personClass.getFields();
+        for(Field f : fields) {
+            System.out.println(f);
+            System.out.println(f.getName());
+        }
+    }
+}
